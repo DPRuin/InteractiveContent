b26d67097df62d5291b4f6efe9850f537cc84b9e
mac126 <mac126@mac126demacbook-air.local>
2018-04-04 14:41:35 +0800

    UIImage图像扩展方法 求平均颜色

--- InteractiveContent/InteractiveContent/Extensions.swift
+++ InteractiveContent/InteractiveContent/Extensions.swift
@@ -11,58 +11,35 @@ import SceneKit
 import ARKit
 
 extension ARSCNView {
-    func averageColorFromEnvironment(at screenPos: SCNVector3) -> SCNVector3 {
-        var colorVector = SCNVector3()
-        
-        // Take screenshot of the scene, without the content
-        scene.rootNode.isHidden = true
-        let screenshot: UIImage = snapshot()
-        scene.rootNode.isHidden = false
-        // Use a patch from the specified screen position
-        let scale = UIScreen.main.scale
-        let patchSize: CGFloat = 100 * scale
-        let screenPoint = CGPoint(x: (CGFloat(screenPos.x) - patchSize / 2) * scale,
-                                  y: (CGFloat(screenPos.y) - patchSize / 2) * scale)
-        let cropRect = CGRect(origin: screenPoint, size: CGSize(width: patchSize, height: patchSize))
-        if let croppedCGImage = screenshot.cgImage?.cropping(to: cropRect) {
-            let image = UIImage(cgImage: croppedCGImage)
-            if let avgColor = image.averageColor() {
-                colorVector = SCNVector3(avgColor.red, avgColor.green, avgColor.blue)
-            }
-        }
-        return colorVector
-    }
-}
-
-extension SCNAnimation {
-    static func fromFile(named name: String, inDirectory: String ) -> SCNAnimation? {
-        let animScene = SCNScene(named: name, inDirectory: inDirectory)
-        var animation: SCNAnimation?
-        animScene?.rootNode.enumerateChildNodes({ (child, stop) in
-            if !child.animationKeys.isEmpty {
-                let player = child.animationPlayer(forKey: child.animationKeys[0])
-                animation = player?.animation
-                stop.initialize(to: true)
-            }
-        })
-        
-        animation?.keyPath = name
-        
-        return animation
-    }
+   
 }
 
 extension UIImage {
+    
+    /// 平均颜色
     func averageColor() -> (red: CGFloat, green: CGFloat, blue: CGFloat)? {
-        if let cgImage = self.cgImage, let averageFilter = CIFilter(name: "CIAreaAverage") {
+        
+        /*
+         CIFilter是CoreImage核心图像的过滤器
+         */
+        if let cgImage = self.cgImage, let averageFilter = CIFilter(name: "CIAverageFilter") {
             let ciImage = CIImage(cgImage: cgImage)
+            // 大小
             let extent = ciImage.extent
+            // 用于CoreImage核心图像过滤器参数
             let ciExtent = CIVector(x: extent.origin.x, y: extent.origin.y, z: extent.size.width, w: extent.size.height)
+            // 输入图像
             averageFilter.setValue(ciImage, forKey: kCIInputImageKey)
+            // 输入范围
             averageFilter.setValue(ciExtent, forKey: kCIInputExtentKey)
+            
+            // 过滤器输出图像
             if let outputImage = averageFilter.outputImage {
+                // 核心图形渲染和分析的上下文
                 let context = CIContext(options: nil)
-                var bitmap = [UInt8](repeating: 0, count: 4)
+                // 创建默认值为0，数量为4的数组
+                var bitmap = [UInt8](repeatElement(0, count: 4))
+                // 交出位图
                 context.render(outputImage,
                                toBitmap: &bitmap,
                                rowBytes: 4,
@@ -71,8 +48,8 @@ extension UIImage {
                                colorSpace: CGColorSpaceCreateDeviceRGB())
                 
                 return (red: CGFloat(bitmap[0]) / 255.0,
-                        green: CGFloat(bitmap[1]) / 255.0,
-                        blue: CGFloat(bitmap[2]) / 255.0)
+                    green: CGFloat(bitmap[1]) / 255.0,
+                    blue: CGFloat(bitmap[2]) / 255.0)
             }
         }
         return nil
