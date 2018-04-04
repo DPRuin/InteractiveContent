f4b115a3a3d023520b2573ccc0670b65f42e0b98
mac126 <tulaomao152@126.com>
2018-04-04 16:18:22 +0800

    SCNAnimation扩展获取模型的动画

--- InteractiveContent/InteractiveContent/Extensions.swift
+++ InteractiveContent/InteractiveContent/Extensions.swift
@@ -39,8 +39,28 @@ extension ARSCNView {
     }
 }
 
-extension CAAnimation {
+extension SCNAnimation {
     
+    /// 获取模型动画
+    static func fromFile(named name: String, inDirectory: String) -> SCNAnimation? {
+        let animScene = SCNScene(named: name, inDirectory: inDirectory)
+        var animation: SCNAnimation?
+        
+        animScene?.rootNode.enumerateChildNodes({ (child, stop) in
+            if !child.animationKeys.isEmpty { // 节点动画key不为空
+                let player = child.animationPlayer(forKey: child.animationKeys[0])
+                animation = player?.animation
+                
+                // 停止
+                stop.initialize(to: true)
+            }
+        })
+        // ？？ 关键路径
+        animation?.keyPath = name
+        
+        return animation
+        
+    }
 }
 
 extension UIImage {
